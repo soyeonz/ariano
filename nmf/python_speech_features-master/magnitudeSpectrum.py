92133985f8a0f7953b8f9eb8afdd7ffe586db653
박세원 <bttb66@bagsewon-ui-MacBook-Air.local>
2017-09-29 14:15:56 +0900

    nmfTensor

--- nmf/python_speech_features-master/magnitudeSpectrum.py
+++ nmf/python_speech_features-master/magnitudeSpectrum.py
@@ -3,25 +3,33 @@ import scipy.io.wavfile as wavfile
 import numpy as np
 import pylab as pl
 
-rate, data = wavfile.read("E1_bass.wav")
-# print(data)
+rate, data = wavfile.read("Butterfly.wav")
 t = np.arange(len(data[:,0]))*1.0/rate
-print(data)
-# merge_y = [x*np.log10(x) if x>=1 else 1 for x in n]
-print(data.shape)
-print(data[100000:1180778,1])
+
+#Original Signal graph
+fig = pl.figure()
+g1 = fig.add_subplot(221)
+g1.set_title("Original signal")
+g1.plot(data)
+
+
 for i in range(0,180778):
   if(data[i,1]>0):
       start = i
       break
 print(start)
 temp = np.abs(np.fft.rfft(data[start:180778,1]))
-# print(data[:2048])
+
 p = [20*np.log10(x) if x>=1 else 1 for x in temp]
 
 
 f = np.linspace(0, rate/2.0, len(p))
-pl.plot(f, p)
-pl.xlabel("Frequency(Hz)")
-pl.ylabel("Power(dB)")
+
+g2 = fig.add_subplot(222)
+g2.set_title("FFT")
+
+g2.plot(f, p)
+# g2.xlabel("Frequency(Hz)")
+# g2.ylabel("Power(dB)")
+
 pl.show()
